59a34a4d2b2e8421facd49d12d86d32bf5a06fb0
Random Facades <random.facades@gmail.com>
2022-07-03 07:16:53 -0600

    Removed all values from dbc overload tables No longer needed as the dbc is being loaded properly Also, client's dbc files need to match, so dbc should be primary source

---
+++ data/sql/custom/db_world/custom_02_3.sql
@@ -0,0 +1,9 @@
+-- 
+-- REMOVE overwrite values for these dbc files
+-- They've been properly loaded from dbc now
+--
+
+TRUNCATE acore_world.gtoctregenhp_dbc;
+TRUNCATE acore_world.gtregenhpperspt_dbc;
+TRUNCATE acore_world.gtregenmpperspt_dbc;
+

23fbf9fdfd89d320d82708ecc31d2b720ab12399
Random Facades <random.facades@gmail.com>
2022-07-10 15:47:45 -0600

    Fix pathing of Rut'theran Village Guard

---
+++ data/sql/custom/db_world/custom_03_1.sql
@@ -0,0 +1,8 @@
+-- 
+-- Fixed pathing of guard in Teldrassil
+--
+
+UPDATE `waypoint_data` SET `position_y` = '876.276', `position_z` = '22.2327' WHERE `id` = '468250' and `point` = '3';
+UPDATE `waypoint_data` SET `position_x` = '8649.74', `position_y` = '872.051', `position_z` = '23.5721' WHERE `id` = '468250' and `point` IN ('2', '28');
+
+
